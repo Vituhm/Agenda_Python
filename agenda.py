3a39a2d2a7cc11d1761000a5631a961e573639cf
Aluno Lab 135 <lab135@lab.senai>
2023-08-30 19:18:13 -0300

    v1.0

---
+++ agenda.py
@@ -0,0 +1,21 @@
+import os
+agenda=[]
+sair=False
+while(sair==False):
+    print("\n---- Agenda Telefônica ----")
+    print("Lista de Opções")
+    print("1 - Novo Contato")
+    print("2 - Listar Contatos")
+    print("3 - Editar Contato")
+    print("4 - Excluir Contato")
+    print("5 - Sair")
+    opcao=int(input("Digite a opção desejada: "))
+
+    if(opcao==1):                                           #Add novo contato
+        novo_contato=[]
+        nome=input("\nDigite o nome do contato:")
+        novo_contato.append(nome)
+        telefone=input("Digite o telefone do contato:")
+        novo_contato.append(telefone)
+        agenda.append(novo_contato)
+        os.system('cls')
